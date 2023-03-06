2e800f41717219e535ae5498faa821cf43975375
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2023-03-06 13:50:06 -0300

    proyecto terminado

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const display   = document.querySelector("display");
+const display   = document.querySelector("#display");
 const buttons   = document.querySelectorAll("button");
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", () =>{
-        if(btn.id=="="){
+        if(btn.id==="="){
             display.value = eval(display.value);
-        } else if (btn.id == "ac"){
+        } else if (btn.id === "ac"){
             display.value = "";
         } else if (btn.id == "de"){
             display.value = display.value.slice(0,-1);
@@ -14,3 +14,4 @@ buttons.forEach((btn) => {
         }
     });
 });
+
